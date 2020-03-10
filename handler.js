cc313a219a06ea91fa403e30a25ef2c4b21d0c70
adrianopb <adrianopbraga1@gmail.com>
2020-03-09 21:37:09 -0300

    api first concious push

--- handler.js
+++ handler.js
@@ -1,59 +1,12 @@
 'use strict';
 
-const express = require('express')
-const sls = require('serverless-http')
-const app = express()
+module.exports.list = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'LIST!',
+    }),
+  };
 
-app.get('/products', async (req, res) => {
-  // try {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: 'Go Serverless v1.0! Your function executed successfully!'
-        }
-      )
-    };
-  // } catch (e) {
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify(
-  //       {
-  //         message: 'An unexpected error has occurred. Please try again later.'
-  //       },
-  //       null,
-  //       2
-  //     )
-  //   }
-  // }
-})
-module.exports.server = sls(app);
-
-// module.exports.products = async event => {
-//   try {
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(
-//         {
-//           message: 'Go Serverless v1.0! Your function executed successfully!'
-//         },
-//         null,
-//         2
-//       )
-//     };
-//   } catch (e) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify(
-//         {
-//           message: 'An unexpected error has occurred. Please try again later.'
-//         },
-//         null,
-//         2
-//       )
-//     }
-//   }
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-// };
+  callback(null, response);
+};
